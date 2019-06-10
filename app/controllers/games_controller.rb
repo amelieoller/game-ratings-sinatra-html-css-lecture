c88e11be0bcd78010eaa4adfea58266f45a4207a
Amelie Oller <amelieoller@gmail.com>
2019-06-10 14:51:38 -0700

    Add game logic in controller and add view files

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,18 @@
 class GamesController < ApplicationController
 
+	get '/games' do
+		@games = Game.all
+		erb :'/games/index'
+	end
+
+	get '/games/new' do
+		erb :'/games/new'
+	end
+
+	post '/games' do
+		@game = Game.create(title: params[:title], description: params[:description])
+
+		redirect "/games"
+	end
+
 end
