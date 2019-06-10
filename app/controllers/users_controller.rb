fa92dadeb42a720d8046b7335d404615c7957780
Amelie Oller <amelieoller@gmail.com>
2019-06-10 14:52:15 -0700

    Create login and signup pages and logic in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,37 @@
 class UsersController < ApplicationController
 
+	get '/signup' do
+    erb :signup
+	end
+	
+	get '/login' do
+		erb :login
+	end
+
+	get '/users/:id' do
+		@user = User.find_by(id: params[:id])
+		erb :'/users/show'
+	end
+
+	post '/users' do
+		@user = User.find_by(email: params[:email])
+
+		if @user
+			redirect '/login'
+		else
+			@user = User.create(email: params[:email], password: params[:password])
+			if @user.save
+				session[:user_id] = @user.id
+				redirect "/users/#{@user.id}"
+			else
+				erb :signup
+			end
+		end
+	end
+
+	get '/logout' do
+		session.clear
+		redirect '/'
+	end
+
 end
--- config.ru
+++ config.ru
@@ -6,3 +6,4 @@ end
 
 run ApplicationController
 use GamesController
+use UsersController
