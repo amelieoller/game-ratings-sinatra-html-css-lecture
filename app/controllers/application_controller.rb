a74f3bec9b38164d5486255c571f282203399ba3
Amelie Oller <amelieoller@gmail.com>
2019-06-10 14:52:30 -0700

    Enable sessions and add current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,16 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "game_rating_app"
   end
 
   get "/" do
     erb :welcome
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
 end
