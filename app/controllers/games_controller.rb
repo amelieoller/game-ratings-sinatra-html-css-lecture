cdb23ae2942b7c23e54cde226be9c4512e7a9bda
Amelie Oller <amelieoller@gmail.com>
2019-06-11 14:12:51 -0700

    Add edit and delete functionality to games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,4 @@
 class GamesController < ApplicationController
-
 	get '/games' do
 		@games = Game.all
 		erb :'/games/index'
@@ -9,10 +8,41 @@ class GamesController < ApplicationController
 		erb :'/games/new'
 	end
 
+	get '/games/:id/edit' do
+		@game = Game.find(params[:id])
+		erb :'/games/edit'
+	end
+
+	get '/games/:id' do
+		@game = Game.find(params[:id])
+		erb :'/games/show'
+	end
+
 	post '/games' do
-		@game = Game.create(title: params[:title], description: params[:description])
+		# @game = Game.create(title: params[:title], description: params[:description])
+		@game = Game.create(params)
+		@game.user = current_user
+		@game.save
 
 		redirect "/games"
 	end
 
+	patch '/games/:id' do
+		@game = Game.find(params[:id])
+		
+		if @game.user_id == current_user.id && params[:title] != "" && params[:description] != "" && params[:image_url] != "" && params[:rating] != ""
+			@game.update(title: params[:title], description: params[:description], image_url: params[:image_url], rating: params[:rating])
+
+			redirect "/games/#{@game.id}"
+    else
+      redirect "users/#{current_user.id}"
+    end
+	end
+	
+  delete '/games/:id' do
+		@game = Game.find(params[:id])
+		@game.destroy
+
+		redirect "games"		
+  end
 end
