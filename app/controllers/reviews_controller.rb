a6310092b8fecb29883c3a25caafdcf145b35338
Amelie Oller <amelieoller@gmail.com>
2019-07-17 12:07:41 -0700

    Remove code for nested forms study group

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,19 +4,6 @@ class ReviewsController < ApplicationController
 	end
 
 	post '/reviews' do
-		game = Game.new(title: params[:game][:title], description: params[:game][:description], image_url: params[:game][:image_url], rating: params[:game][:rating])
 
-		game.user = current_user
-
-		params[:game][:reviews].each do |review_details|
-			review = Review.new(review_details)
-			review.user = current_user
-			review.game = game
-
-			review.save
-		end
-
-		game.save
-		redirect "/games"
 	end
 end
